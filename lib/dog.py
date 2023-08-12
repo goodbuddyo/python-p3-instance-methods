c55fab2a8f4cd0daa8c6c54f949cedbe079ab5b0
Seano <sean01234@yahoo.com>
2023-08-12 09:01:34 -0400

    add methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+# 1. Add method `sit()` to `Dog` class that will print
+# "The dog is sitting."
 
 class Dog:
     # Class body goes here
+    # Instance method definition
+    def sit(self):
+        print('The dog is sitting.')
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print('Woof!')
+
+
+spot = Dog()
+spot.sit()
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-
+# 2. Define a `Person` class in `lib/person.py`
+# 3. Add an instance method `talk()` to your Person class that will print "Hello World!"
+# 4. Add an instance method `walk()` to your Person class that will print "The person is walking."
 class Person:
     # Class body goes here
+    # Instance method definition
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+
+joe = Person()
+joe.talk()
